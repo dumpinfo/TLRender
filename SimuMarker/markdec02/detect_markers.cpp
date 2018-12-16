acd9459a988410b25f7240fe239d456e0f72d059
speech <twtravel@126.com>
2018-12-17 06:03:03 +0800

    add files

--- SimuMarker/markdec02/detect_markers.cpp
+++ SimuMarker/markdec02/detect_markers.cpp
@@ -51,8 +51,41 @@ bool isRotationMatrix(Mat &R)
 }
 
 /**
+ * 功能： 1. 通过给定的欧拉角计算对应的旋转矩阵
+ * 作者： KKtech
+ * 日期： 2017-10-12
+**/
+Mat eulerAnglesToRotationMatrix(Vec3f &theta)
+{
+    // 计算旋转矩阵的X分量
+    Mat R_x = (Mat_<double>(3,3) <<
+               1,       0,              0,
+               0,       cos(theta[0]),   -sin(theta[0]),
+               0,       sin(theta[0]),   cos(theta[0])
+               );
+
+    // 计算旋转矩阵的Y分量
+    Mat R_y = (Mat_<double>(3,3) <<
+               cos(theta[1]),    0,      sin(theta[1]),
+               0,               1,      0,
+               -sin(theta[1]),   0,      cos(theta[1])
+               );
+
+    // 计算旋转矩阵的Z分量
+    Mat R_z = (Mat_<double>(3,3) <<
+               cos(theta[2]),    -sin(theta[2]),      0,
+               sin(theta[2]),    cos(theta[2]),       0,
+               0,               0,                  1);
+
+    // 合并 
+    Mat R = R_z * R_y * R_x;
+
+    return R;
+}
+ 
+/**
  * 功能： 1. 通过给定的旋转矩阵计算对应的欧拉角
- * 作者： Zuo
+ * 作者： KKtech
  * 日期： 2017-10-12
 **/
 Vec3f rotationMatrixToEulerAngles(Mat &R)
@@ -76,6 +109,15 @@ Vec3f rotationMatrixToEulerAngles(Mat &R)
     return Vec3f(x, y, z);   
 }
  
+ void Rotation2Quanternion(Mat &R) {
+	double x,y,z,w;
+	w =  sqrt(1.0 + R.at<double>(0,0) + R.at<double>(1,1) + R.at<double>(2,2)) / 2.0;
+	double w4 = (4.0 * w);
+	x = (R.at<double>(2,1) - R.at<double>(1,2) ) / w4 ;
+	y = (R.at<double>(0,2) - R.at<double>(2,0) ) / w4 ;
+	z = (R.at<double>(1,0) - R.at<double>(0,1) ) / w4 ;
+	printf("%lf, %lf, %lf, %lf\n", w, x,y,z);
+}
 
 /**
  */
@@ -220,6 +262,7 @@ int main(int argc, char *argv[]) {
 				 printf("%.3lf,%.3lf,%.3lf\n", double(eulervec[0]*180.0/3.1415926),
 				                               double(eulervec[1]*180.0/3.1415926),
                                                double(eulervec[2]*180.0/3.1415926));
+				Rotation2Quanternion(rotation_matrix);
 				 //trans printf("%.3lf,%.3lf,%.3lf\n", double(tvecs[i][0]),double(tvecs[i][1]),double(tvecs[i][2]));
 				}
             }
